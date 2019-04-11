08f31213d9131f0a06d3a63150fc529cd604c66e
Matt Kleven <klevennb115@gmail.com>
2019-04-11 01:35:29 -0500

    Created timer and drawing board

--- src/App.js
+++ src/App.js
@@ -3,26 +3,35 @@ import Square from './Square';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: 100
+    }
+  }
   populateBoxes =()=>{
     let box = <Square />;
     let arr = [box];
-    for (let index = 0; index < 100; index++) {
+    for (let index = 0; index < 1090; index++) {
       arr.push(box)
     }
     return arr.map(
       (boxes, i) => boxes
     )
   }
-  boxes = () =>{
-    console.log('hi');
-    
-    return 
+  startGame = () =>{
+    this.timerID = setInterval(
+      () => this.setState({timer: this.state.timer - 1}),
+      1000
+    );
   }
   render() {
     return (
       <div className="App">
-        <div><h1>Community Square Art</h1></div>
-        <div>
+        <div><h1>Box Fill Game</h1></div>
+        <div><h2>Time Left:{this.state.timer}</h2></div>
+        <div><button onClick={this.startGame}>Start Game</button></div>
+        <div className='display-area'>
           {this.populateBoxes()}
         </div>
       </div>
--- src/Square.js
+++ src/Square.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
 
 class Square extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            color : 'square-white'
+        }
+    }
+   
+
+    changeColor = () => {
+        if (this.state.color === 'square-black') {
+            this.setState({ color: 'square-white' })  
+        } else {
+            this.setState({color:'square-black'}) 
+        }
+        
+    }
     render(){
         return(
-            <div className='square-white'>
+            <div className={this.state.color} onMouseOver={this.changeColor}>
             </div>
         )
     }
